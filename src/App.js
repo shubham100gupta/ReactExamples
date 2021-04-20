45a807cae98fa8b60e9dd75a1680e9320d149b13
shubham100gupta <gupta.shubham@neosoftmail.com>
2021-04-20 20:13:51 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
+import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
+import Welcome from './component/welcome'
+import Greet from './component/Greet'
+import Message from './component/Message'
+import Counter from './component/Counter'
+import FunctionClick from './component/FunctionClick'
+import ClassClick from './component/ClassClick'
+import ParentComponent from './component/ParentComponent'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( < div className = "App" >
+        <
+        ParentComponent / >
+
+
+
+        {
+            /* <
+                    FunctionClick / >
+                    <
+                    ClassClick / > */
+        }
+
+
+        {
+            /* <
+                    Counter / > */
+        }
+
+        {
+            /* <
+                    Message / > */
+        }
+
+        {
+            /* 
+                    <
+                    Greet name = "shubham"
+                    heroname = "batman" / >
+                    <
+                    button > Action < /button> <
+                    Greet name = "ishika"
+                    heroname = "wonder women" / >
+                    <
+                    button > action < /button>
+
+
+
+                    <
+                    Welcome name = "rohan"
+                    heroname = "ironman" / >
+                    <
+                    button > action < /button>
+
+                    <
+                    Welcome name = "satvik"
+                    heroname = "spiderman" / >
+                    <
+                    button > action < /button> */
+        }
+
+
+
+
+
+
+
+
+
+        <
+        /div>
+
+    );
 }
 
-export default App;
+export default App
